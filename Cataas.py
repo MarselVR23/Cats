a1c5457ea47f5bab2efbe4fa12b35765e4af97b9
MarselVR23 <valievm.r@mail.ru>
2025-08-26 22:10:42 +0500

    Создана функция для получения изображения с котиком

--- Cataas.py
+++ Cataas.py
@@ -5,6 +5,19 @@ from io import BytesIO
 
 from Scripts.Kalkulyator import window
 
+
+def loade_image():
+    try:
+        response = request.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return Image.PhotoImage(img)
+    except Exception as e:
+        print(f"Произлшда ошибка: {e}")
+        return None
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x400")

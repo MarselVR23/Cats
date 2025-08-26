5a7debdec509317bfdeca5ea4f699bcd78b584d2
MarselVR23 <valievm.r@mail.ru>
2025-08-26 23:16:11 +0500

    Вместо кнопки добавлено меню

--- Cataas.py
+++ Cataas.py
@@ -3,9 +3,6 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from Scripts.Kalkulyator import window
-
-
 def load_image(url):
     try:
         response = requests.get(url)
@@ -19,24 +16,37 @@ def load_image(url):
         return None
 
 def set_image():
+    global img
+    url = "https://cataas.com/cat"
+    img = load_image(url)
     if img:
         label.config(image=img)
         label.image = img
+
+def exit():
+    window.destroy()
 
 
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
-label = Label()
+label = Label(window)
 label.pack()
 
-update_button = Button(text="Обновить", command=set_image)
-update_button.pack()
+# update_button = Button(window, text="Обновить", command=set_image)
+# update_button.pack()
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
 img = load_image(url)
-
 set_image()
 
 window.mainloop()
